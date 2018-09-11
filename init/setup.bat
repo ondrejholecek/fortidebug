0c1a1e7f1a800b0bc0d497525ad3a094ea91f299
Ondrej Holecek <ondrej@holecek.eu>
2018-09-11 16:08:59 +0200

    allow running the ps1 file

--- init/setup.bat
+++ init/setup.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
+@ECHO Preparing environment to initialize the FortiMonitor application
+
 @REM Save current execution policy
 powershell -Command Get-ExecutionPolicy -Scope CurrentUser >temp.txt
 set /p OEXP=<temp.txt
@@ -8,7 +10,11 @@ del temp.txt
 powershell -Command Set-ExecutionPolicy -Scope CurrentUser unrestricted
 
 @REM Run the setup powershell script
+powershell -Command Unblock-File -Path scripts\windows.ps1
+
+@ECHO Starting the init script
 powershell -File scripts\windows.ps1
 
 @REM Restore the original execution policy
+@ECHO Restoring the environment
 powershell -Command Set-ExecutionPolicy -Scope CurrentUser %OEXP%
