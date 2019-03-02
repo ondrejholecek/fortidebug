e41645f5d9013cc619488b93b9acc18c834a038f
Ondrej Holecek <ondrej@holecek.eu>
2019-03-02 17:59:48 +0100

    interrupt (soft+hard) utilization

---
+++ parsers/Interrupts.py
@@ -0,0 +1,199 @@
+from EasyParser import EasyParser
+
+import re
+import time
+
+
+# FG1K2D-2 # diag hardware sysinfo interrupts
+#             CPU0       CPU1       CPU2       CPU3       CPU4       CPU5       CPU6       CPU7
+#    0:         36          0          0          0          0          0          0          0   IO-APIC-edge      timer
+#    2:          0          0          0          0          0          0          0          0    XT-PIC-XT-PIC    cascade
+#    3:          0    3577171          0          0          0          0          0          0   IO-APIC-edge      serial
+#    4:          0       4688          0          0          0          0          0          0   IO-APIC-edge      serial
+#    8:          0          0          0          0          0          0          0          0   IO-APIC-edge      rtc
+#   16:          0    1832355          0          0          0          0          0          0   IO-APIC-fasteoi   ehci_hcd:usb1, ehci_hcd:usb2, uhci_hcd:usb5, uhci_hcd:usb9, linux-kernel-bde, mgmt1
+#   17:          0          0          3          0          0          0          0          0   IO-APIC-fasteoi   uhci_hcd:usb3, uhci_hcd:usb6, mgmt2
+#   18:          0          0          0          0          0          0          0          0   IO-APIC-fasteoi   uhci_hcd:usb4, uhci_hcd:usb7
+#   19:          0          0          0          0          0          0          0          0   IO-APIC-fasteoi   uhci_hcd:usb8, net2280
+#   64:          1          0          0     260298          0          0          0          0   PCI-MSI-edge      ahci
+#   65:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_0_vpn0
+#   66:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_0_vpn1
+#   67:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_0_vpn2
+#   68:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_0_vpn3
+#   69:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_0_kxp
+#   70:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_1_vpn0
+#   71:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_1_vpn1
+#   72:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_1_vpn2
+#   73:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_1_vpn3
+#   74:          0          0          0          0          0          0          0          0   PCI-MSI-edge      cp8_1_kxp
+#   75:          5          1          0          0          0          0          0          0   PCI-MSI-edge      np6_0-tx-rx0
+#   76:          0          1          5          0          0          0          0          0   PCI-MSI-edge      np6_0-tx-rx1
+#   77:          0          0          1          0          5          0          0          0   PCI-MSI-edge      np6_0-tx-rx2
+#   78:          0          0          0          0          0          0          0          0   PCI-MSI-edge      np6_0-err0
+#   79:          0          0         17          0          0          0          0          0   PCI-MSI-edge      np6_0-nturbo-tx-rx0
+#   80:          0          0          0          0          0          0          0          0   PCI-MSI-edge      np6_0-nturbo-err0
+#   81:   16418964          0          0          1          0          0          0          0   PCI-MSI-edge      np6_0-nturbo-ips-0
+#   82:          0   16141636          0          1          0          0          0          0   PCI-MSI-edge      np6_0-nturbo-ips-1
+#   83:          0          0          0   14991882          0          0          0          0   PCI-MSI-edge      np6_0-nturbo-ips-2
+#   84:          0          0          0          1   15879562          0          0          0   PCI-MSI-edge      np6_0-nturbo-ips-3
+#   85:          0          0          0          0          1   16707050          0          0   PCI-MSI-edge      np6_0-nturbo-ips-4
+#   86:          0          0          0          0          1          0   16444822          0   PCI-MSI-edge      np6_0-nturbo-ips-5
+#   87:          0          0          0          0          1          0          0   16581448   PCI-MSI-edge      np6_0-nturbo-ips-6
+#   88:          0          0          0          0          0          0          0          0   PCI-MSI-edge      np6_0-nturbo-ips-7
+#   89:          0          0          0          0          0          1          7          0   PCI-MSI-edge      np6_0-tx-rx3
+#   90:          5          0          0          0          0          1          0          0   PCI-MSI-edge      np6_0-tx-rx4
+#   91:          0          0          5          0          0          1          0          0   PCI-MSI-edge      np6_0-tx-rx5
+#   92:          0          0          0          0          0          0          0          0   PCI-MSI-edge      np6_0-err1
+#   93:          0          0          0          0          0          0          0          0   PCI-MSI-edge      np6_0-nturbo-err1
+#   94:  207221826          0          0          0          0          0          1          0   PCI-MSI-edge      np6_1-tx-rx0
+#   95:          0          0  200639569          0          0          0          1          0   PCI-MSI-edge      np6_1-tx-rx1
+#   96:          0          0          0          0  240962811          0          1          0   PCI-MSI-edge      np6_1-tx-rx2
+#   97:          0          0          0          0          0          0          0          0   PCI-MSI-edge      np6_1-err0
+#   98:          0          1  479259756          0          0          0          0          0   PCI-MSI-edge      np6_1-nturbo-tx-rx0
+#   99:          0          0          0          0          0          0          0          0   PCI-MSI-edge      np6_1-nturbo-err0
+#  100:          0          0          1          0          0          0  240663469          0   PCI-MSI-edge      np6_1-tx-rx3
+#  101:  210887756          0          1          0          0          0          0          0   PCI-MSI-edge      np6_1-tx-rx4
+#  102:          0          0  202674599          0          0          0          0          0   PCI-MSI-edge      np6_1-tx-rx5
+#  103:          0          0          0          0          0          0          0          0   PCI-MSI-edge      np6_1-err1
+#  104:          0          0          0          0          0          0          0          0   PCI-MSI-edge      np6_1-nturbo-err1
+#  NMI:     451378     451332     451379     451331     451379     451330     451380     451329   Non-maskable interrupts
+#  LOC:   27025393   27025374   27025356   27025338   27025320   27025302   27025284   27025266   Local timer interrupts
+#  SPU:          0          0          0          0          0          0          0          0   Spurious interrupts
+#  PMI:     451378     451332     451379     451331     451379     451330     451380     451329   Performance monitoring interrupts
+#  IWI:          0          0          0          0          0          0          0          0   IRQ work interrupts
+#  RES:   54764029   23029410   66355685   21516202   64664597   18859876   69639605   20136217   Rescheduling interrupts
+#  CAL:       1227       1315       1304        287       1295       1290       1323       1325   Function call interrupts
+#  TLB:        350        792       1188       1324        712        547        831        507   TLB shootdowns
+#  ERR:          0
+#  MIS:          0
+
+# FG1K2D-2 # fnsysctl cat /proc/softirqs
+#                     CPU0       CPU1       CPU2       CPU3       CPU4       CPU5       CPU6       CPU7
+#           HI:          0          0          0          0          0          0          0          0
+#        TIMER:   28521064   28525832   28520649   28526326   28524819   28526243   28524655   28526254
+#       NET_TX:        994      57592        871        518        854        502        578        462
+#       NET_RX:  576621254    1990912  889144076          0  350281983          2  353098308          0
+#        BLOCK:        476        301        193     275534        181        396         98        313
+# BLOCK_IOPOLL:          0          0          0          0          0          0          0          0
+#      TASKLET:   14128586    1943262   12439627    1942008    9747759    1944864    9735439    1961939
+#        SCHED:    9818324   13579287   11060339   13505914   10051866   12468454    9796770   12164434
+#      HRTIMER:          0          0          0          0          0          0          0          0
+#          RCU:   26288609   14045430   23576147   14059434   19574070   15025426   19446047   15275527
+
+class ParserInterrupts(EasyParser):
+	def prepare(self):
+		self.re_cpus      = re.compile("^\s+CPU.*(\d+)\s+\n")
+
+	def get(self, soft=True, hard=True):
+		interrupts   = {}
+		collected_on = None
+		cpus         = None
+
+		if hard:
+			hw = self.get_real('hard')
+			interrupts.update(hw['interrupts'])
+			collected_on = hw['collected_on']
+			cpus         = hw['cpus']
+
+		if soft:
+			sw = self.get_real('soft')
+			interrupts.update(sw['interrupts'])
+			collected_on = sw['collected_on']
+			cpus         = sw['cpus']
+
+		if collected_on == None or cpus == None: 
+			raise Exception('Either soft or hard interrupts must be selected')
+
+		return {
+			'collected_on': collected_on,
+			'cpus'        : cpus,
+			'interrupts'  : interrupts,
+		}
+
+	def get_real(self, source):
+		if source == 'hard':
+			interrupts = self.sshc.clever_exec("diagnose hardware sysinfo interrupts")
+		elif source == 'soft':
+			interrupts = self.sshc.clever_exec("fnsysctl cat /proc/softirqs")
+		else:
+			raise Exception('Interrupts can be either "hard" or "soft"')
+
+		command_time = time.time()
+		result = {}
+
+		# count cpus
+		g = self.re_cpus.search(interrupts)
+		if g == None: raise Exception("Cannot count CPUs")
+		cpus = int(g.group(1))+1
+
+		# parse lines with entry for each cpu
+		tmp = "^\s*(\S+):" + "\s*(\d+)"*cpus
+		if source == 'hard': tmp += "\s+(.*?)[\r]*$"
+		re_interrupt = re.compile(tmp, re.M)
+
+		for iline in re_interrupt.findall(interrupts):
+			if source == 'hard':
+				try: int(iline[0])
+				except ValueError: itype = 'other'
+				else: itype = 'numeric'
+
+				if itype == 'numeric':
+					tmp = iline[-1].split(None, 1)
+					trigger = tmp[0]
+					desc    = tmp[1]
+	
+				elif itype == 'other':
+					trigger = 'other'
+					desc    = iline[-1]
+			
+			elif source == 'soft':
+				itype = 'soft'
+				trigger = 'other'
+				if iline[0] == 'NET_RX':
+					desc = 'Incoming packets (NAPI)'
+				elif iline[0] == 'NET_TX':
+					desc = 'Outgoing packets (NAPI)'
+				elif iline[0] == 'HI':
+					desc = 'High priority tasklet'
+				elif iline[0] == 'TASKLET':
+					desc = 'Normal priority tasklet'
+				elif iline[0] in ('TIMER', 'SCHED', 'RCU', 'HRTIMER'):
+					desc = 'Kernel housekeeping'
+				elif iline[0] in ('BLOCK', 'BLOCK_IOPOLL'):
+					desc = 'Block device (disk)'
+				else:
+					desc = 'softirq'
+
+			ticks = {'total':0}
+			for i in range(cpus):
+				ticks[i] = int(iline[1+i])
+				ticks['total'] += ticks[i]
+
+			result[iline[0]] = {
+				'type'        : itype,
+				'trigger'     : trigger,
+				'description' : desc,
+				'ticks'       : ticks,
+				'source'      : source,
+			}
+
+		# parse lines with single cpu column
+		re_single = re.compile('^\s*(ERR|MIS):\s*(\d+)', re.M)
+		for single in re_single.findall(interrupts):
+			ticks = {'total': int(single[1])}
+			for i in range(cpus): ticks[i] = ticks['total']
+
+			result[single[0]] = {
+				'type'        : 'single',
+				'trigger'     : 'other',
+				'description' : 'unknown',
+				'ticks'       : ticks,
+				'source'      : source,
+			}
+
+		return {
+			'collected_on': command_time,
+			'cpus'        : cpus,
+			'interrupts'  : result,
+		}
+
---
+++ utilities/inttop.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python2.7
+
+import os
+
+# to be able to import our modules from the directory above
+os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from parsers.CurrentTime import ParserCurrentTime
+from parsers.Interrupts import ParserInterrupts
+from parsers.ProcessCPU import ParserProcessCPU
+from _common import ssh, cycle, simple_command_with_timestamp, prepend_timestamp
+
+import re
+import sys
+
+sshc, args = ssh([
+	{ 'name':'--collect-time',   'type':int, 'default':5,  'help':'How long should each cycle take' },
+	{ 'name':'--max',   'type':int, 'default':45,  'help':'Maximum lines to show (45 by default)' },
+	{ 'name':'--summarize', 'default':False, 'action':'store_true',  'help':'Summarize ticks from all CPUs' },
+	{ 'name':'--cpu', 'type':int, 'default':[], 'action':'append',  'help':'Summarize ticks from selected CPUs' },
+	{ 'name':'--no-soft', 'default':False, 'action':'store_true',  'help':'Do not show softirqs' },
+	{ 'name':'--no-hard', 'default':False, 'action':'store_true',  'help':'Do not show hardirqs' },
+	{ 'name':'--hz',           'type':int, 'default':100,  'help':'CONFIG_HZ of device, do not change' },
+], """
+""")
+
+def difference_per_second(old, new, time_difference):
+	if 'interrupts' not in old or 'interrupts' not in new: raise Exception('Invalid structures for calculating difference')
+	if 'collected_on' not in old or 'collected_on' not in new: raise Exception('Invalid structures for calculating difference')
+	if 'cpus' not in old or 'cpus' not in new: raise Exception('Invalid structures for calculating difference')
+	if new['cpus'] != old['cpus']: raise Exception('Number of CPUs has changed')
+
+	diff = {}
+	for k in new['interrupts'].keys():
+		if k not in old['interrupts']: continue
+
+		diff[k] = {}
+		for cpu in new['interrupts'][k]['ticks'].keys():
+			diff[k][cpu] = int(round(float(new['interrupts'][k]['ticks'][cpu] - old['interrupts'][k]['ticks'][cpu])/time_difference))
+
+		diff[k]['source'] = new['interrupts'][k]['source']
+
+	return diff
+
+
+def sort_interrupts(diff, cpus='each', ignore_zero=True):
+	ss = []
+
+	for k in diff.keys():
+		if cpus == 'each':
+			for cpu in diff[k].keys():
+				if type(cpu) != int: continue
+				ss.append( ((k, cpu), diff[k][cpu]) )
+
+		elif cpus == 'total':
+			ss.append( ((k, 'total'), diff[k]['total']) )
+
+		elif type(cpus) == tuple:
+			summary = 0
+			for cpu in cpus:
+				summary += diff[k][cpu]
+			ss.append( ((k, cpus), summary) )
+			
+	ret = []
+	for s in sorted(ss, key=lambda x: x[1], reverse=True):
+		if ignore_zero and (s[1] == 0): continue
+		ret.append(s)
+	return ret
+
+def do(sshc, cache, max_lines, display_type, hz, soft, hard):
+	etime = ParserCurrentTime(sshc).get()
+	ints  = ParserInterrupts(sshc).get(soft=soft, hard=hard)
+	usage = ParserProcessCPU(sshc).get([])
+	
+	if 'last' not in cache:
+		cache['last'] = {
+			'interrupts': ints,
+			'cpu' : usage,
+		}
+		return
+
+	time_difference = ints['collected_on'] - cache['last']['interrupts']['collected_on']
+
+	overall_cpus = {}
+	for tmp in ['user', 'system', 'idle', 'iowait', 'irq', 'softirq']:
+		overall_cpus[tmp] = int(round(((usage['global'][tmp] - cache['last']['cpu']['global'][tmp])*100)/(time_difference*hz)))
+
+	diff = difference_per_second(cache['last']['interrupts'], ints, time_difference)
+	diff_sorted_keys = sort_interrupts(diff, display_type)
+	if max_lines != 0: diff_sorted_keys = diff_sorted_keys[:max_lines]
+
+	total_ticks_soft = sum([diff[x]['total'] for x in diff.keys() if diff[x]['source'] == 'soft'])
+	total_ticks_hard = sum([diff[x]['total'] for x in diff.keys() if diff[x]['source'] == 'hard'])
+
+	if os.name == 'nt':
+		os.system('cls')
+		print "Interrupt lines utilization    (written by Ondrej Holecek <oholecek@fortinet.com>)"
+	else:
+		print "\x1b[2J\x1b[H\033[1mInterrupt lines utilization    (written by Ondrej Holecek <oholecek@fortinet.com>)\033[0m"
+
+	
+	filters_applied = "Applied filters: "
+	if type(display_type) == tuple: filters_applied += "CPU[" + ",".join([str(x) for x in sorted(display_type)]) + "] "
+	elif display_type == 'total': filters_applied += "CPU[total] "
+	elif display_type == 'each': filters_applied += "CPU[separate] "
+	if max_lines != 0: filters_applied += "TOP[%i] " % (max_lines,)
+	if soft and hard: filters_applied += "TYPE[soft,hard] "
+	elif soft: filters_applied += "TYPE[soft] "
+	elif hard: filters_applied += "TYPE[hard] "
+
+	print prepend_timestamp("Overall CPU utilization: %3.1f %% user, %3.1f %% system, %3.1f %% idle" % (
+		overall_cpus['user'], overall_cpus['system'], overall_cpus['idle'],
+	), etime, 'inttop')
+	print prepend_timestamp("Overall CPU utilization: %3.1f %% iowait, %3.1f %% irq, %3.1f %% softirq" % (
+		overall_cpus['iowait'], overall_cpus['irq'], overall_cpus['softirq'],
+	), etime, 'inttop')
+	print prepend_timestamp(filters_applied, etime, 'inttop')
+	print prepend_timestamp("%-11s %5s %9s %10s %4s  %s" % ("LINE", "SOURCE", "CPU(s)", "RUNS", "PERC", "DESCRIPTION",), etime, 'inttop')
+
+	for k in diff_sorted_keys:
+		((iname, itype), iticks) = k
+		source = ints['interrupts'][iname]['source']
+		desc = ints['interrupts'][iname]['description']
+		if len(desc) > 30: desc = desc[:25] + "[...]"
+		if type(itype) == tuple: itype = 'selected'
+
+		if source == 'soft':
+			perc = (iticks*100)/total_ticks_soft
+			source_a = 'S'
+		elif source == 'hard':
+			perc = (iticks*100)/total_ticks_hard
+			source_a = 'H'
+
+		print prepend_timestamp("%-16s %1s %9s %10i %4i  %s" % (iname, source_a, itype, iticks, perc, desc,), etime, 'inttop')
+	
+	cache['last'] = {
+		'interrupts': ints,
+		'cpu': usage,
+	}
+	sys.stdout.flush()
+
+if __name__ == '__main__':
+	itype = 'each'
+	if args.summarize: itype = 'total'
+	elif len(args.cpu) > 0: itype = tuple(args.cpu)
+
+	soft = True
+	if args.no_soft: soft = False
+	hard = True
+	if args.no_hard: hard = False
+
+	cache = {}
+	try:
+		cycle(do, {
+			'sshc': sshc, 
+			'cache': cache,
+			'max_lines': args.max,
+			'display_type': itype,
+			'hz': args.hz,
+			'soft': soft,
+			'hard': hard,
+		}, args.collect_time, cycles_left=[args.max_cycles], debug=args.debug)
+	except KeyboardInterrupt:
+		sshc.destroy()
+
