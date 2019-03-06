dd8b69860e9554f1d48207526afeaf0c2129ada3
Ondrej Holecek <ondrej@holecek.eu>
2019-03-06 15:04:36 +0100

    option to write output to individial files

--- utilities/continuous_command.py
+++ utilities/continuous_command.py
@@ -18,6 +18,7 @@ sshc, args = ssh([
 	{ 'name':'--vdom',         'default':None, 'help':'VDOM to use when executing the command' },
 	{ 'name':'--mgmt-vdom',    'default':False, 'action':'store_true',  'help':'Use management VDOM to execute the command' },
 	{ 'name':'--grep',         'default':None, 'help':'Show only lines matching the regular expression' },
+	{ 'name':'--directory-output', 'default':None, 'help':'Dump each command output as a separate file in a specified directory' },
 ], """
 Execute specific command every `--cycle-time`. 
 
@@ -32,7 +33,7 @@ is used as the VDOM name.
 Optionally you can use `--grep` option to only print the lines matching the regular expression.
 """)
 
-def do(sshc, commands, vdom, grep):
+def do(sshc, commands, vdom, grep, directory):
 	etime = ParserCurrentTime(sshc).get()
 	
 	for command in commands:
@@ -45,15 +46,33 @@ def do(sshc, commands, vdom, grep):
 			this_command = g.group(2)
 			if this_vdom.lower() == 'global': this_vdom = None
 
+		out = sshc.clever_exec(this_command, this_vdom)
 		if grep == None:
-			print simple_command_with_timestamp(sshc, etime, this_command, info=this_command, vdom=this_vdom)
+			output(sshc, out, etime, this_command, directory)
 		else:
-			out = sshc.clever_exec(this_command, this_vdom)
 			for line in out.split("\n"):
 				if not grep.search(line): continue
-				print prepend_timestamp(line, etime, this_command)
+				output(sshc, line, etime, this_command, directory)
 	
 		sys.stdout.flush()
+	
+def output(sshc, txt, etime, command_name, directory):
+	# Writing to stdout is simple
+	if directory == None:
+		print prepend_timestamp(txt, etime, command_name)
+		return
+	
+	# If directory is specified, write the output per-file to the directory
+	info = sshc.get_info()
+	hostname = info['hostname']
+	if info['hostname_extension'] != None: hostname += info['hostname_extension']
+	hostname = re.sub('[^a-zA-Z0-9-]', ' ', hostname).strip().replace(' ', '_')
+
+	fname = "%s/%s.%s.%s" % (directory, hostname, command_name.replace(' ', '_'), etime.as_timestamp(),)
+	with open(fname, "wb") as f:
+		f.write(txt)
+
+	print prepend_timestamp("Saved to %s" % (fname,), etime, command_name)
 
 if __name__ == '__main__':
 	#
@@ -78,6 +97,7 @@ if __name__ == '__main__':
 			'commands': args.command,
 			'vdom': vdom,
 			'grep': grep,
+			'directory': args.directory_output,
 		}, args.cycle_time, cycles_left=[args.max_cycles], debug=args.debug)
 	except KeyboardInterrupt:
 		sshc.destroy()
